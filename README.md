cb66f5d2e2bf75083d5f7958ab9b4cd1d0be7234
Stephen Caldwell <steve@campusexplorer.com>
2017-07-11 15:27:54 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+PawsX::DynamoDB::DocumentClient - Blah blah blah
+
+# SYNOPSIS
+
+    use PawsX::DynamoDB::DocumentClient;
+
+# DESCRIPTION
+
+PawsX::DynamoDB::DocumentClient is
+
+# AUTHOR
+
+Steve Caldwell <scaldwell@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2017- Steve Caldwell
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/PawsX/DynamoDB/DocumentClient.pm
@@ -0,0 +1,39 @@
+package PawsX::DynamoDB::DocumentClient;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+PawsX::DynamoDB::DocumentClient - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use PawsX::DynamoDB::DocumentClient;
+
+=head1 DESCRIPTION
+
+PawsX::DynamoDB::DocumentClient is
+
+=head1 AUTHOR
+
+Steve Caldwell E<lt>scaldwell@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2017- Steve Caldwell
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
